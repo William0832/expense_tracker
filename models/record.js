c1003713d4918bc7b45e7135fbf581d9d72946ce
William0832 <squall0832@gmail.com>
2020-03-15 22:45:43 +0800

    add recod, user models and creat seeder

---
+++ models/record.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const recordSchema = new Schema({
+  name: {
+    type: String,
+    require: true
+  },
+  category: {
+    type: String,
+    require: true
+  },
+  amount: {
+    type: Number,
+    require: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    index: true,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Record', recordSchema)
---
+++ models/seed/seeder.js
@@ -0,0 +1,112 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const Record = require('../record')
+const User = require('../user')
+const usersData = [
+  {
+    name: 'user1',
+    email: 'user1@example.com',
+    password: '12345678'
+  },
+  {
+    name: 'user2',
+    email: 'user2@example.com',
+    password: '12345678'
+  }
+]
+const recordsData = [
+  {
+    name: '早餐',
+    category: '餐飲食品',
+    amount: 60,
+    date: '2020/03/02'
+  },
+  {
+    name: '午餐',
+    category: '餐飲食品',
+    amount: 60,
+    date: '2020/03/02'
+  },
+  {
+    name: '晚餐',
+    category: '餐飲食品',
+    amount: 100,
+    date: '2020/03/04'
+  },
+  {
+    name: '書',
+    category: '休閒娛樂',
+    amount: 300,
+    date: '2020/03/10'
+  },
+  {
+    name: '加油:汽車',
+    category: '交通出行',
+    amount: 1000,
+    date: '2020/03/10'
+  },
+  {
+    name: '捷運',
+    category: '交通出行',
+    amount: 90,
+    date: '2020/03/15'
+  },
+  {
+    name: '電影',
+    category: '休閒娛樂',
+    amount: 300,
+    date: '2020/03/11'
+  },
+  {
+    name: '演唱會',
+    category: '休閒娛樂',
+    amount: 3000,
+    date: '2020/03/11'
+  },
+  {
+    name: '租金',
+    category: '居家物業',
+    amount: 10000,
+    date: '2020/03/15'
+  }
+]
+
+mongoose.connect('mongodb://localhost/recod', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('db error')
+})
+db.once('open', () => {
+  console.log('db connected!')
+
+  // add users in DB
+  const users = []
+  usersData.forEach(user => {
+    users.push(new User(user))
+  })
+
+  // add hash password in User
+  users.forEach(newUser => {
+    bcrypt.genSalt(10, (err, salt) => {
+      if (err) return console.log(err)
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) return console.log(err)
+        newUser.password = hash
+        newUser.save(err => {
+          if (err) console.log(err)
+        })
+      })
+    })
+  })
+
+  // add record in DB
+  recordsData.forEach(record => {
+    let index = Math.floor(Math.random() * usersData.length)
+    record.userId = users[index]._id
+    Record.create(record)
+  })
+  console.log('Adding sample is done')
+})
